df4e55e8f5c0d446425e812426155a825ef62911
Kadda Benmebarek <149805211+Kaddabenmebarek@users.noreply.github.com>
2024-06-18 09:02:51 +0200

    Update SynthesisOrderService.java

--- src/main/java/org/research/kadda/labinventory/service/SynthesisOrderService.java
+++ src/main/java/org/research/kadda/labinventory/service/SynthesisOrderService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 public class SynthesisOrderService {
     private SynthesisOrderRepository synthesisOrderRepository;
 
-    @Autowired
     public SynthesisOrderService(SynthesisOrderRepository cSOrderRepository) {
         this.synthesisOrderRepository = cSOrderRepository;
     }
