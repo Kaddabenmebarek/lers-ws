7ce9764ec8d875a8deb841c782f407e948420c4d
Kadda Benmebarek <149805211+Kaddabenmebarek@users.noreply.github.com>
2024-06-18 09:02:43 +0200

    Update ResoptionService.java

--- src/main/java/org/research/kadda/labinventory/service/ResoptionService.java
+++ src/main/java/org/research/kadda/labinventory/service/ResoptionService.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 public class ResoptionService {
     private ResoptionRepository resoptionRepository;
 
-    @Autowired
     public ResoptionService(ResoptionRepository resoptionRepository) {
         this.resoptionRepository = resoptionRepository;
     }
